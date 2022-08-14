130da1d3987fc9986df238ae48d995104e626239
arkmadj <ahmadjinadu@gmail.com>
2022-08-14 15:39:58 +0100

    Added bit and memmory and floats

--- main.go
+++ main.go
@@ -16,4 +16,14 @@ func main() {
 
 	fmt.Println(ageOne, ageTwo, ageThree)
 
+	// bits and meemory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint16 = 21312
+
+	var decOne float32 = 3.14
+	var decTwo float64 = 34234342343243.14
+
+	fmt.Println(numOne, numTwo, numThree, decOne, decTwo)
+
 }
