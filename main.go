9e9343a846e5aadd1b45c1bbe415822174f8b4ac
arkmadj <ahmadjinadu@gmail.com>
2022-08-14 15:32:24 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nameOne string = "Zultan"
+	var nameTwo = "Ahmad"
+	var nameThree string
+	nameFous := "Adekunle"
+
+	fmt.Println(nameOne, nameTwo, nameThree, nameFous)
+
+}
