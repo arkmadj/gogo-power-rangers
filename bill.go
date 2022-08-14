fa186d4947fa29f8d00029bf30ee6dfb1ae37d83
arkmadj <ahmadjinadu@gmail.com>
2022-08-14 21:29:13 +0100

    Added switch user input

--- bill.go
+++ bill.go
@@ -12,14 +12,14 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{"coffee": 2.50, "tea": 1.50},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
 	return b
 }
 
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
@@ -28,7 +28,17 @@ func (b bill) format() string {
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "tip:", b.tip)
+
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(item string, price float64) {
+	b.items[item] = price
 }
--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - Add item, s - Save bill, t - Add tip):", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Enter item name: ", reader)
+		price, _ := getInput("Enter item price: ", reader)
+		fmt.Println(name, price)
+	case "s":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+		fmt.Println(tip)
+	case "t":
+		fmt.Println("You chose t")
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
+
+}
+
 func main() {
-	myBill := newBill("Zultan's bill")
+	myBill := createBill()
+	promptOptions(myBill)
 
-	myBill.format()
-	fmt.Println(myBill.format())
-
+	fmt.Println(myBill)
 }
